1c1fdb4ca74504aecf58e5f6deec7e518b55bc38
pratikjadhav718 <pratikjadhav718@gmail.com>
2021-12-26 23:51:30 +0530

    26-12-2021

--- js/script1.js
+++ js/script1.js
@@ -1,12 +1,51 @@
 async function makeApiRequest(){
     try{
 
-        let response = await fetch("https://gateway.marvel.com:443/v1/public/characters?apikey=dc037e83e85a37c1b65faa0088cb34f9&hash=01007301e25245ea228aee74a98ca911&ts=1&nameStartsWith=sp");
+
+
+        let response = await fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=dc037e83e85a37c1b65faa0088cb34f9&hash=01007301e25245ea228aee74a98ca911&ts=1&nameStartsWith=sp`);
 
         let data = await response.json();
         console.log('data:', data);
 
+        showData(data);
+
     }catch(err){
         console.log('err:', err);
     }
+}
+
+
+function showData(data){
+    console.log('data:', data);
+
+    var x = document.getElementById("search_reasult");
+
+    x.innerHTML = "";
+
+    console.log(data.data.results[0].name);
+
+    for(let i=0; i<data.data.results.length; i++){
+        console.log(data.data.results[i].name);
+
+        let div = document.createElement("div");
+        div.classList.add("each_reasult");
+
+        // div.setAttribute("id", "id_you_like");
+        div.setAttribute("id",  data.data.results[i].id);
+
+        div.onclick = function()  {getId(this.id)}
+        
+        let ptag = document.createElement("p");
+        ptag.innerText = data.data.results[i].name;
+
+        div.appendChild(ptag);
+
+        x.appendChild(div);
+    }
+
+}
+
+function getId(id){
+    console.log(id);
 }
